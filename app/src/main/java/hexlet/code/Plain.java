f6930b30494e52270b3c4b7c59aa22ec823c60a1
gpiento <zzzest@gmail.com>
2023-10-29 23:41:49 +0300

    fix Plain

--- app/src/main/java/hexlet/code/Plain.java
+++ app/src/main/java/hexlet/code/Plain.java
@@ -1,10 +1,32 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Plain {
 
     public static String render(Map<String, Object> mapReadValue1, Map<String, Object> mapReadValue2) {
-        return "Plain";
+
+        StringBuilder diff = new StringBuilder("{\n");
+
+        TreeMap<String, Object> commonMap = new TreeMap<>(mapReadValue1);
+        commonMap.putAll(mapReadValue2);
+
+        for (Map.Entry<String, Object> entry : commonMap.entrySet()) {
+            String key = entry.getKey();
+            Object value1 = mapReadValue1.get(key);
+            Object value2 = mapReadValue2.get(key);
+
+            if (value1 == null) {
+                diff.append(String.format("Property '%s' was added with value: %s%n", key, value2));
+            } else if (value2 == null) {
+                diff.append(String.format("Property '%s' was removed%n", key));
+            } else if (!value1.equals(value2)) {
+                diff.append(String.format("Property '%s' was updated. From '%s' to '%s'%n", key, value1, value2));
+            }
+        }
+        diff.append("}\n");
+
+        return diff.toString();
     }
 }
