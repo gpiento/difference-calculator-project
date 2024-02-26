a5b3d90bf6e62d5f26e746332c01f05c9051d6da
gpiento <zzzest@gmail.com>
2024-02-26 15:11:29 +0300

    change Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,5 +1,10 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,5 +28,29 @@ public class Parser {
         }*/
 
         return new HashMap<>();
+    }
+
+    public static Map<String, Object> textToJson(String data) throws JsonProcessingException {
+
+        if (data == null) {
+            return new HashMap<>();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(data, new TypeReference<>() {
+        });
+    }
+
+    public static Map<String, Object> textToYaml(String data) throws JsonProcessingException {
+
+        if (data == null) {
+            return new HashMap<>();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+        return objectMapper.readValue(data, new TypeReference<>() {
+        });
     }
 }
