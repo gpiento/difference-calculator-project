cd7dbafc5b028a5cecda3e639acef1e665ba132b
gpiento <zzzest@gmail.com>
2024-02-28 23:07:08 +0300

    change DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -40,25 +40,4 @@ public class DifferTest {
         String expectedResult = Files.readString(pathJsonExpected);
         assertEquals(expectedResult, Differ.generate(pathJson1, pathJson2, "json"));
     }
-
-    @Test
-    void generateTest() throws Exception {
-
-        String filePath1 = "src/test/resources/file1.json";
-        String filePath2 = "src/test/resources/file2.json";
-        String expectedResult = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }
-                """;
-
-        String result = Differ.generate(filePath1, filePath2, "stylish");
-
-        //assertEquals(expectedResult, result);
-    }
 }
