33990cd5520a6f6eddb289623da58681ef1ecaea
gpiento <zzzest@gmail.com>
2024-02-28 21:52:17 +0300

    change Json

---
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -0,0 +1,21 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
+
+public class Json {
+
+    public static String formatJson(List<Map<String, Object>> data) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            return mapper.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
