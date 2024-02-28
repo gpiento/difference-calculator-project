557b66440677523cf6cd4644dad46fa11808fe12
gpiento <zzzest@gmail.com>
2024-02-29 01:05:15 +0300

    change DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,19 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    private Path pathPlainExpected = Path.of("src/test/resources/expected/plainTest.txt")
+    private final Path pathPlainExpected = Path.of("src/test/resources/expected/plainTest.txt")
             .toAbsolutePath().normalize();
-    private Path pathStylishExpected = Path.of("src/test/resources/expected/stylishTest.txt")
+    private final Path pathStylishExpected = Path.of("src/test/resources/expected/stylishTest.txt")
             .toAbsolutePath().normalize();
-    private Path pathJsonExpected = Path.of("src/test/resources/expected/jsonTest.txt");
-    private String pathJson1 = "src/test/resources/file1.json";
-    private String pathJson2 = "src/test/resources/file2.json";
-    private String pathYaml1 = "src/test/resources/file1.yaml";
-    private String pathYaml2 = "src/test/resources/file2.yaml";
+    private final Path pathJsonExpected = Path.of("src/test/resources/expected/jsonTest.txt");
+    private final String pathJson1 = "src/test/resources/file1.json";
+    private final String pathJson2 = "src/test/resources/file2.json";
 
     @Test
     void generatePlainTest() throws Exception {
         String expectedResult = Files.readString(pathPlainExpected);
+        String pathYaml1 = "src/test/resources/file1.yaml";
+        String pathYaml2 = "src/test/resources/file2.yaml";
         assertEquals(expectedResult, Differ.generate(pathYaml1, pathYaml2, "plain"));
     }
 
