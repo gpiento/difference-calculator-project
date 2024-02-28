65cd16d1405ce8f017b4810800ddffff21726104
gpiento <zzzest@gmail.com>
2024-02-28 21:51:16 +0300

    change Plain

---
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -0,0 +1,52 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+public class Plain {
+
+    public static String formatPlain(List<Map<String, Object>> data) {
+
+        StringBuilder builder = new StringBuilder("{\n");
+
+        for (Map<String, Object> map : data) {
+            switch (map.get("type").toString()) {
+                case "unchange" -> {
+                    builder.append(String.format("  %s: %s\n", map.get("key"), stringify(map.get("value1"))));
+                }
+                case "add" -> {
+                    builder.append(String.format("  + %s: %s\n", map.get("key"), stringify(map.get("value1"))));
+                }
+                case "remove" -> {
+                    builder.append(String.format("  - %s: %s\n", map.get("key"), stringify(map.get("value1"))));
+                }
+                case "change" -> {
+                    builder.append(String.format("  - %s: %s\n", map.get("key"), stringify(map.get("value1"))));
+                    builder.append(String.format("  + %s: %s\n", map.get("key"), stringify(map.get("value2"))));
+                }
+                default -> {
+                    throw new RuntimeException("Unknown type: " + map.get("type"));
+                }
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private static String stringify(Object value) {
+
+        if (value == null) {
+            return "null";
+        }
+
+        if (value instanceof String) {
+            return "'" + value + "'";
+        }
+
+        if (value instanceof Map || value instanceof List) {
+            return "[complex value]";
+        }
+
+        return value.toString();
+    }
+}
